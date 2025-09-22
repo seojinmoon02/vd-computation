1c12290ede2eb82573c2a69f69b27202fe7023b3
seojinmoon <fastturtle12@snu.ac.kr>
2025-09-23 00:49:53 +0900

    added kirby

---
+++ kirby/sketch.js
@@ -0,0 +1,73 @@
+function setup() {
+  createCanvas(400, 400);
+  background('#2339a8');
+  noStroke()
+}
+
+
+function draw() {
+
+    //stars
+fill('#fef6abff');
+circle(150,70,5)
+circle(60,190,5)
+circle(90,230,5)
+circle(80,225,5)
+circle(80,260,5)
+circle(300,100,5)
+circle(300,170,5)
+circle(350,170,5)
+circle(309,150,5)
+circle(270,320,5)
+circle(260,310,5)
+circle(160,360,5)
+circle(130,80,5)
+
+  //face
+fill('#f0c1d8');
+  circle(200,200,150)
+
+//arms
+  circle(130,180,50)
+  circle(265,162,50)
+
+//mouth
+fill('#d20852')
+  circle(200,190,10)
+
+
+//eyes
+fill('black');
+  circle(185,157,20);
+  circle(185,160,22);
+  circle(185,165,23);
+  circle(185,170,23);
+  circle(185,175,22);
+
+  fill('#2339a8');
+  circle(185,177,20);
+
+fill('black');
+  circle(215,157,20);
+  circle(215,160,22);
+  circle(215,165,23);
+  circle(215,170,23);
+  circle(215,175,22);
+
+  fill('#2339a8');
+  circle(215,177,20);
+
+fill('white')
+  circle(185,157,14);
+  circle(215,157,14);
+
+//feet
+fill('#d20852')
+circle(260,240,90)
+circle(140,280,90)
+
+fill('#2339a8');
+rect(90,280,100,50)
+
+
+}
